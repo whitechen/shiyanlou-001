6a2dc6798fc12da8de1805fee33b728407d5bd2b
Yangbin chen <shucyb@163.com>
2018-01-14 17:50:49 +0800

    learning the list example

--- helloworld.py
+++ helloworld.py
@@ -6,3 +6,13 @@ Created on Wed Jan 10 22:42:51 2018
 """
 
 print("Hello World!")
+
+mybicycles = ['trek', 'cannondale', 'redline', 'specialized']
+mybicycles.append('永久')
+mybicycles.append('凤凰')
+
+mymessage = "My first bicycle was a " + mybicycles[0].title() + "."
+
+print(mymessage)
+
+print(mybicycles)
