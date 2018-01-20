6e9c98a7efcbf7310993d36c8eb7a9b06825f081
Yangbin Chen <ndcyb@263.net>
2018-01-20 18:50:19 +0800

    Add errorhandler for Flask

--- flask-test-app/app.py
+++ flask-test-app/app.py
@@ -5,6 +5,9 @@ app.config.update({
     'SECRET_KEY': 'a random string'
     })
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'),404
 
 @app.route('/')
 def index():
@@ -13,7 +16,8 @@ def index():
 
 @app.route('/user/<username>')
 def user_index(username):
-    
+    if username == 'invalid':
+        abort(404)
     resp = make_response(render_template('user_index.html', username=username))
     resp.set_cookie('username', username)
     return resp
